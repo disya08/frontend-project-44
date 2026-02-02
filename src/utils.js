9e6696e5227fb458ea3224897319410bb706185b
Denis Ivanov <disatrophic@gmail.com>
2026-02-02 22:38:24 +0700

    wew

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,16 @@
-const getRandomInt = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  };
-  
-  export default getRandomInt;
+﻿export const getRandomInt = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+export const getRandomOperator = () => {
+  const operators = ['+', '-', '*'];
+  return operators[Math.floor(Math.random() * operators.length)];
+};
+
+export const generateProgression = (start, step, length) => {
+  const progression = [];
+  for (let i = 0; i < length; i += 1) {
+    progression.push(start + i * step);
+  }
+  return progression;
+};
